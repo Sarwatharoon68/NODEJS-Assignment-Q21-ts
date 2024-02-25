27568e35f6cd5bd838a4a8d67995e6210f435d7d
Sarwat Haroon <159713603+Sarwatharoon68@users.noreply.github.com>
2024-02-25 11:02:53 +0500

    Add files via upload

---
+++ q21.ts
@@ -0,0 +1,6 @@
+/*Ques # 21:
+Let think ofsomething you could store in a Typescript Object.Write a program that creates object containing these items*/
+
+
+let person : {Name : string, fatherName : string, Age : number} = {Name : "Sarwat", fatherName : " Haroon", Age : 19}
+console.log(person);
